b8d8eeadc0edd1c4f9e28bd74f9a30152b3597aa
Emre Çakır <ceamkrier@gmail.com>
2017-11-18 12:42:05 +0100

    Green
    ConsoleViewTest -> shouldAskUserBetAmount test case succeed.

--- src/game/ConsoleView.java
+++ src/game/ConsoleView.java
@@ -7,13 +7,16 @@ public class ConsoleView {
 	final String WELCOME = "< Welcome to casino >\n\n"
 			+ "Here is your chance to make some serious money!\n"
 			+ "All you need to do is start betting with our generous gift of 10 coins.\n"
-			+ "Don't forget to follow the given instructions to understand how the game works!\n";
+			+ "Don't forget to follow the given instructions to understand how the game works!\n"
+			+ "Good luck and have fun!\n";
 	public void showWelcomeMessage() {
 		PrintStream out = System.out;
 		out.print(WELCOME);
 	}
 	
 	public String askUserBetAmount() {
+		PrintStream out = System.out;
+		out.print("Enter your bet");
 		return "";
 	}
 
